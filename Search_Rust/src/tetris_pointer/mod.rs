a097f45df940f0cfd714fa4b2b48f4ff2589bb48
punarist-test <136059874+aki-com@users.noreply.github.com>
2024-11-18 21:47:18 +0900

    note

--- Search_Rust/src/tetris_pointer/mod.rs
+++ Search_Rust/src/tetris_pointer/mod.rs
@@ -3,17 +3,20 @@ mod app_read;
 
 pub struct Player {
     number: u32,
-    Field0Ptr: usize
+    Field0Ptr: [usize; 20]
 }
 impl Player {
-    fn fieldRead
+    fn fieldRead(&self) -> 
 }
 pub fn new(number: u32) -> Player {
     let Option = app_read::new("Tetris_UI.exe");
     let offsets = vec![0x007E9000, 0x0, 0xE0, 0x1F0, 0x58, 0x0];
-    let address = Option.read_memory_chain(&offsets).unwrap();
-    let value = 0;
-    println!("Value: {}", value);
+    let address0 = Option.read_memory_chain(&offsets).unwrap()+0x40;
+    let mut address :[usize; 20] = [0; 20];
+    for i in 0..20 {
+        address[i] = address0;
+    }   
+
     Player {
         number: number,
         Field0Ptr: address
